acfed98fea571321b10317af1403388bea957e0a
Sean Larkin <sean.larkin@cuw.edu>
2017-07-22 22:18:53 -0700

    feat(uglify): add ability for uglifyjs to be caught and options validated to ensure sourcemaps are generated

--- src/index.js
+++ src/index.js
@@ -6,9 +6,16 @@ import { formatProcessedSourceMaps, getWritePathForSerializedData } from 'bundle
 import chalk from 'chalk';
 import logoLogger from './utils';
 
+const HIDDEN_WARNING_INFO_FOOTER = 'To hide this warning set {warnings: false} inside the BundleBuddyWebpackPlugin configuration options.';
 const INVALID_SOURCEMAP_WARNING = chalk.yellow(`[BundleBuddyWebpackPlugin] WARNING: config.options.devtool was not set to 'source-map'. Enabling full sourcemaps for bundle-buddy.
-  To hide this warning set {warnings: false} inside the BundleBuddyWebpackPlugin configuration options.
+  ${HIDDEN_WARNING_INFO_FOOTER}
 `);
+const UGLIFYJS_WEBPACK_PLUGIN_WARNING = chalk.yellow(`[BundleBuddyWebpackPlugin] WARNING: UglifyJsWebpackPlugin has been detected.
+  Setting {sourceMap: true} for UglifyJsWebpackPlugin options to ensure that sourcemaps are generated. bundle-buddy will fail if it this setting is not enabled.
+  ${HIDDEN_WARNING_INFO_FOOTER}
+`);
+
+const UGLIFY_NAME_REGEX = /uglify/i;
 
 export default class BundleBuddyWebpackPlugin {
   constructor(options) {
@@ -16,9 +23,11 @@ export default class BundleBuddyWebpackPlugin {
     this.sam = pluginOptions.sam;
     this.warnings = pluginOptions.warnings;
   }
+
   apply(compiler) { // eslint-disable-line
     const getUserConfigOptions = () => compiler.options;
     const isFullSourceMap = () => getUserConfigOptions().devtool === 'source-map';
+    const hasUglyJsPlugin = () => getUserConfigOptions().plugins.some(plugin => UGLIFY_NAME_REGEX.test(plugin.constructor.name));
 
     if (!isFullSourceMap()) {
       if (this.warnings) {
@@ -26,6 +35,15 @@ export default class BundleBuddyWebpackPlugin {
       }
 
       compiler.options.devtool = 'source-map'; // eslint-disable-line
+    }
+
+    if (hasUglyJsPlugin()) {
+      const uglifyPluginOptions = getUserConfigOptions().plugins.find(plugin => UGLIFY_NAME_REGEX.test(plugin.constructor.name)).options;
+      const { sourceMap } = uglifyPluginOptions;
+      if (!sourceMap && this.warnings) {
+        console.log(UGLIFYJS_WEBPACK_PLUGIN_WARNING); // eslint-disable-line
+        uglifyPluginOptions.sourceMap = true;
+      }
     }
 
     const getOutDirFor = () => {
